37600a2ddf917486491db24b7f4501cc57b559ce
head-node <76886903+head-node@users.noreply.github.com>
2021-03-10 15:51:22 +0530

    no name

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,12 @@
 // write code for Welcome component here
+import React from "react";
+
+function message(props) {
+  return (
+    <>
+      <h1>Hey {props.name}!</h1>
+      <h2>Welcome to Newton School.</h2>
+    </>
+  );
+}
+export default message;
